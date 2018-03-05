2f602491e7904295bed79e0a1d59d7ccce4c2629
Quoc-Cuong Tran <tranquoccuong@outlook.com.vn>
2018-03-05 08:29:19 -0800

    Update negativeQInt.cpp

--- negativeQInt.cpp
+++ negativeQInt.cpp
@@ -2,10 +2,10 @@
 
 QInt negativeQInt(const QInt &a) {
         QInt b;
-        for (int i = 0; i < 4; ++i)
+        for (int i = 0; i < N; ++i)
                 b.data[i] = ~a.data[i];
         ++b.data[0];
-        for (int i = 0; i < 3; ++i)
+        for (int i = 0; i < N - 1; ++i)
                 if (b.data[i] == 0) //overflow
                         ++b.data[i + 1];
         return b;
