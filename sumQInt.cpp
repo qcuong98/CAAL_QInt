2bac1e6af85bb3ea334ce44125dd00cf4142ec20
CUONG TRAN <tranquoccuong@outlook.com.vn>
2018-03-05 23:35:48 +0700

    fix bugs

--- sumQInt.cpp
+++ sumQInt.cpp
@@ -5,7 +5,7 @@ QInt sumQInt(const QInt &a, const QInt &b) {
         bool tmp = 0;
         for (int i = 0; i < N; ++i) {
                 bool new_tmp = (a.data[i] <= INT32_MAX - b.data[i] - tmp && 
-                                b.data[i] != INT32_MAX - tmp);
+                                b.data[i] <= INT32_MAX - tmp);
                 c.data[i] = a.data[i] + b.data[i] + tmp;
                 tmp = new_tmp;
         }
