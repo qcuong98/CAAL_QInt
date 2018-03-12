6b572ce768b943d2f5134ccccf3a330197a8e4e9
CUONG TRAN <tranquoccuong@outlook.com.vn>
2018-03-12 20:28:43 +0700

    fix bug

--- logicOps.cpp
+++ logicOps.cpp
@@ -23,3 +23,14 @@ QInt operator ~ (const QInt &a) {
     for (int i=0; i<N; i++) res.data[i] = ~a.data[i];
     return res;
 }
+
+QInt operator - (const QInt &a) {
+        QInt b = ~a;
+        ++b.data[0];
+        for (int i = 0; i < N - 1; ++i)
+                if (b.data[i] == 0) //overflow
+                        ++b.data[i + 1];
+		else
+			break;
+        return b;
+}
