0e6a3ae77a98fa7b54186ed5260412655c1ebfc4
Vuong Hy <vuonghy2442@hotmail.com.vn>
2018-03-19 21:29:46 +0700

    fix bug multiplication

--- mulQInt.cpp
+++ mulQInt.cpp
@@ -6,9 +6,9 @@ QInt operator *(const QInt &a, const QInt &b) {
 
 	for (int s = N - 1; s >= 0; s--) 
 		for (int i = 31; i >= 0; i--) {
-			if ((b.data[s] >> i) & 1)
-				res = res + b;
 			res = res << 1;
+			if ((b.data[s] >> i) & 1)
+				res = res + a;
 		}
 	return res;
 
